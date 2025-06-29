c08024ed548429fa840c55926c69f145a9f7d09a
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-06-29 14:31:24 -0400

    Done

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    public static String read(String key) {
+        String filePath = "Files/Config.properties";
+        Properties properties = new Properties();
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            properties.load(fis);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: ");
+
+        } catch (IOException io) {
+            System.out.println("Error reading the file: ");
+        }
+
+        // print the value associated with the key "userName"
+        return properties.getProperty(key);
+    }
+}
+

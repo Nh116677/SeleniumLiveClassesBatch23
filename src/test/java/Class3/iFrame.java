dac82bbc2eabd01cae49f4d6e726a9997b8d27e1
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-01 18:14:01 -0400

    Done

--- src/test/java/Class3/iFrame.java
+++ src/test/java/Class3/iFrame.java
@@ -1,11 +1,12 @@
-package Class3;
+package liveClas03;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class iFrame {
+public class frames {
     public static void main(String[] args) {
         WebDriver driver=new ChromeDriver();
         driver.get("https://syntaxprojects.com/handle-iframe-homework.php");
--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -1,36 +1,34 @@
-package utils;
+package utlis;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-
-
 public class CommonMethods {
 
-    public void OpenBrowserAndLaunchApplication(String browserName, String url) {
+
+    public  void OpenBrowserAndLaunchApplication(String browserName, String url){
         WebDriver driver;
 
-        String browserName = ConfigReader.read("browser");
-        switch (browserName) {
+        switch (browserName){
             case "Chrome":
-                 driver = new ChromeDriver();
+                driver=new ChromeDriver();
                 break;
             case "fireFox":
-
-                driver = new FirefoxDriver();
+                driver=new FirefoxDriver();
                 break;
             case "edge":
-
-               driver = new EdgeDriver();
+                driver=new EdgeDriver();
                 break;
 
             default:
-                throw new RuntimeException("Invalid Browser Name")
+                throw new RuntimeException("Invalid Browser Name");
+
         }
 
         driver.manage().window().maximize();
         driver.get(url);
     }
+
 }
